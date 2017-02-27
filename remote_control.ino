70378c285ab50be0905546805523838d878efa08
kevinforsythe <kevin.forsythe@gmail.com>
2017-02-26 18:56:59 -0800

    first commit

---
+++ remote_control.ino
@@ -0,0 +1,110 @@
+/*
+  Remote Controller
+ 
+ Crude prototype sketch for the magellan bot remote control.
+ Has a selector switch which enables/disables the deadman switch (DMS)
+ and a momentary button which must be held down during DMS mode for 
+ the bot to continue moving autonomously.
+ */
+
+
+// constants for pin numbers
+const int dmsEnablePin = 9;       // pin for DMS enable switch
+const int dmsDeadLED = 8;         // pin to signal DMS 'Dead' state
+const int dmsAliveLED = 7;        // pin to signal DMS 'Alive' state
+const int dmsLiveButton = 6;      // pin to receive 'StayAlive' button signal
+const int dmsDisabledLED = 5;     // pin to signal DMS is disabled
+const int haltLED = 4;            // pin to signal 'Halt' state is engaged
+const int haltButton = 6;         // pin to receive 'Halt' button signal
+const int manualControlPin = 10;  // pin to enable joystick manual control
+const int joystickXPin = A0;      // pin for joystick left/right
+const int joystickYPin = A1;      // pin for joystick forward/reverse
+
+
+// variables
+int dmsEnableState = 0;           // variable for whether the DMS is enabled
+int dmsButtonState = 0;           // variable for whether the DMS button is depressed
+int haltState = 1;                // variable for current halt state
+int haltButtonState = 0;          // variable for reading halt button
+int manualControlState = 0;       // variable for manual control
+int joystickXValue = 0;           // variable for left/right
+int joystickYValue = 0;           // variable for forward/reverse
+
+
+void setup() {
+  // initialize GPIO pins for their functions
+  pinMode(dmsEnablePin, INPUT);
+  pinMode(dmsDeadLED, OUTPUT);
+  pinMode(dmsAliveLED, OUTPUT);
+  pinMode(dmsLiveButton, INPUT);
+  pinMode(dmsDisabledLED, OUTPUT);
+  pinMode(haltLED, OUTPUT);
+  //pinMode(haltButton, INPUT);
+  pinMode(joystickXPin, INPUT);
+  pinMode(joystickYPin, INPUT);
+
+  // initialize serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  // first check if the DMS enable switch is on
+  dmsEnableState = digitalRead(dmsEnablePin);
+  
+  if (dmsEnableState == HIGH) {
+    Serial.print("DMS is enabled.");
+    digitalWrite(dmsDisabledLED, LOW);
+    digitalWrite(haltLED, LOW);
+
+    // now check if DMS button is depressed
+    dmsButtonState = digitalRead(dmsLiveButton);
+
+    // if DMS is enabled and DMS button is depressed, cancel 'Die' signal and send 'Alive' signal
+    if (dmsButtonState == HIGH) {
+      digitalWrite(dmsDeadLED, LOW);
+      digitalWrite(dmsAliveLED, HIGH);
+      Serial.print("\t DMS button is closed (stay alive!)\n");
+    } else {
+      // if DMS is enabled and DMS is not depressed, cancel 'Alive' signal and send 'Die' signal
+      digitalWrite(dmsAliveLED, LOW);
+      digitalWrite(dmsDeadLED, HIGH);
+      Serial.print("\t DMS button is open (Die!)\n");
+    }
+  }
+  if (dmsEnableState == LOW) {
+    
+    // if the DMS is not enabled, turn off DMS LEDs, turn on 'DMS OFF' indicator 
+    digitalWrite(dmsDisabledLED, HIGH);
+    digitalWrite(dmsAliveLED, LOW);
+    digitalWrite(dmsDeadLED, LOW);
+    Serial.print("DMS is disabled.");
+    
+    // check if halt button has been pushed, if so, togglet halt state
+    haltButtonState = digitalRead(haltButton);
+    if (haltButtonState == HIGH) {
+      haltState = !haltState;
+      delay(500);
+      }
+    digitalWrite(haltLED, haltState);
+    if (haltState == 1) {
+      Serial.print("\t HALT!");
+    }
+    // check if manual control switch is on
+    manualControlState = digitalRead(manualControlPin);
+    if (manualControlState == LOW) {
+      Serial.print("\t Manual Control Disabled.");
+    } else {
+      Serial.print("\t Manual Control Enabled.");
+    }
+    // if DMS is off and manual control switch is on and not halted, read and report joystick values
+    if (manualControlState == HIGH and haltState == LOW) {
+      joystickXValue = analogRead(joystickXPin);
+      joystickYValue = analogRead(joystickYPin);
+      Serial.print("\t CW/CCW: ");
+      Serial.print(joystickXValue);
+      Serial.print(",\t Forward/Reverse: ");
+      Serial.print(joystickYValue);
+    }
+  Serial.print("\n");
+  }
+ }
